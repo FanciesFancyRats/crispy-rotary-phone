37b44d49cb44c106355428e321903f5bac6fe06e
FanciesFancyRats <magriffi1986@gmail.com>
2018-04-21 12:49:43 -0500

    look up escapse sequences sometime

--- 600game/js/main.js
+++ 600game/js/main.js
@@ -4,16 +4,33 @@ var GameState = {
 
 	preload:function() {
 		this.game.load.image('hold','images/placeholder.jpg');
+		this.game.load.text('words', 'words/words.txt');
 },
 
 	create:function() {
 		this.game.add.image(0,0,'hold');
-		console.log('create');
+		var rawText = game.cache.getText('words');
+		this.dictionary = rawText.split('\n');
+		var myword = 'AAH';
+		this.checkWord(myword);
+
 },
 
 	update:function() {
+		
 
 },
+	checkWord:function(guess){
+		for(i = 0; i < 7; i++){
+			console.log(this.dictionary[i])
+			if(guess === this.dictionary[i]){
+				console.log('!!!!!!!!!!!!!!!!!!!!!!!!!');	
+			}
+		}
+		console.log(this.dictionary.includes(guess));
+},
+	
+
 
 }
 var game = new Phaser.Game(800, 600, Phaser.AUTO);
