e22d20fb2187450d77179d71e2ce9621da2b6020
FanciesFancyRats <magriffi1986@gmail.com>
2018-04-22 18:45:23 -0500

    deals a hand

--- 600game/js/main.js
+++ 600game/js/main.js
@@ -3,21 +3,52 @@
 var GameState = {
 
 	preload:function() {
-		this.game.load.image('hold','images/placeholder.jpg');
-		this.game.load.text('words', 'words/words.txt');
+		this.game.load.image("hold","images/placeholder.jpg");
+		this.game.load.text("words", "words/words.txt");
+		this.game.load.image("BG", "images/bg.png");
+		this.game.load.image("A", "images/A.png");
+		this.game.load.image("B", "images/B.png");
+		this.game.load.image("C", "images/C.png");
+		this.game.load.image("D", "images/D.png");
+		this.game.load.image("E", "images/E.png");
+		this.game.load.image("F", "images/F.png");
+		this.game.load.image("G", "images/G.png");
+		this.game.load.image("H", "images/H.png");
+		this.game.load.image("I", "images/I.png");
+		this.game.load.image("J", "images/J.png");
+		this.game.load.image("K", "images/K.png");
+		this.game.load.image("L", "images/L.png");
+		this.game.load.image("M", "images/M.png");
+		this.game.load.image("N", "images/N.png");
+		this.game.load.image("O", "images/O.png");
+		this.game.load.image("P", "images/P.png");
+		this.game.load.image("Q", "images/Q.png");
+		this.game.load.image("R", "images/R.png");
+		this.game.load.image("S", "images/S.png");
+		this.game.load.image("T", "images/T.png");
+		this.game.load.image("U", "images/U.png");
+		this.game.load.image("V", "images/V.png");
+		this.game.load.image("W", "images/W.png");
+		this.game.load.image("X", "images/X.png");
+		this.game.load.image("Y", "images/Y.png");
+		this.game.load.image("Z", "images/Z.png");
+		
+
 },
 
 	create:function() {
-		this.cons = 'BCDFGHJKLMNPQRSTVWXYZ';
-		this.cons = this.cons.split('');
-		this.vowl = 'AEIOU';
-		this.vowl = this.vowl.split('');
-		this.game.add.image(0,0,'hold');
-		var rawText = game.cache.getText('words');
-		this.dictionary = rawText.split('\n');
-		var myword = 'AAH';
-		console.log(this.checkWord(myword));
+		this.letters = game.add.group();
+		this.hand = [];
+		this.cons = "BCDFGHJKLMNPQRSTVWXYZ";
+		this.cons = this.cons.split("");
+		this.vowel = "AEIOU";
+		this.vowel = this.vowel.split("");
+		this.game.add.image(0,0,"hold");
+		var rawText = game.cache.getText("words");
+		this.dictionary = rawText.split("\n");
 		this.dealHand(32);
+
+		this.createHand();
 
 },
 
@@ -31,12 +62,40 @@ var GameState = {
 },
 	dealHand:function(handSize){
 		//Generates a hand based on handSize
-			
+		vowelNum = Math.ceil(handSize/3);
+		constNum = handSize - vowelNum;
+		for (i = 0; i < vowelNum; i++){
+			this.hand.push(this.vowel[Math.floor(Math.random()*5)]);
+		}
+		for (i = 0; i < constNum; i++){
+			this.hand.push(this.cons[Math.floor(Math.random()*(this.cons.length))]);
+		}	
+},
+	createHand:function(){
+		//Takes the contents of this.hand and makes game objects letters
+		var letter;
+		for( i = 0; i < this.hand.length; i++){
+			letter = this.letters.create(0, 0, this.hand[i]);
+			letter.anchor.setTo(0.5);
+			letter.inputEnabled = true;
+			letter.events.onInputDown.add(this.addLetter, this);
+			letter.name = (this.hand[i] + i);
+		}
+		this.letters.align(8, -1, 100, 100);
+		this.letters.x = (this.game.world.centerX - 400);
+		this.letters.y = (this.game.world.centerY - 50);
+
+},
+	addLetter:function(letter){
+		//Adds clicked letter to the displayArray
+		console.log(letter.name)		
+		letter.kill();
 	}
+
 	
 
 
 }
-var game = new Phaser.Game(800, 600, Phaser.AUTO);
-game.state.add('GameState', GameState);
-game.state.start('GameState');
+var game = new Phaser.Game(1280, 720, Phaser.AUTO);
+game.state.add("GameState", GameState);
+game.state.start("GameState");
