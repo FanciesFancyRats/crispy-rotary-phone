8d80b88c013646ae6293dd40b5e4f52eb18f2e2f
FanciesFancyRats <magriffi1986@gmail.com>
2018-04-22 11:25:04 -0500

    swaping

--- 600game/js/main.js
+++ 600game/js/main.js
@@ -8,11 +8,16 @@ var GameState = {
 },
 
 	create:function() {
+		this.cons = 'BCDFGHJKLMNPQRSTVWXYZ';
+		this.cons = this.cons.split('');
+		this.vowl = 'AEIOU';
+		this.vowl = this.vowl.split('');
 		this.game.add.image(0,0,'hold');
 		var rawText = game.cache.getText('words');
 		this.dictionary = rawText.split('\n');
 		var myword = 'AAH';
-		this.checkWord(myword);
+		console.log(this.checkWord(myword));
+		this.dealHand(32);
 
 },
 
@@ -21,14 +26,13 @@ var GameState = {
 
 },
 	checkWord:function(guess){
-		for(i = 0; i < 7; i++){
-			console.log(this.dictionary[i])
-			if(guess === this.dictionary[i]){
-				console.log('!!!!!!!!!!!!!!!!!!!!!!!!!');	
-			}
-		}
-		console.log(this.dictionary.includes(guess));
+		//Takes the word [guess] and returns true or false wether or not it is in this.dictionary
+		return(this.dictionary.includes(guess));
 },
+	dealHand:function(handSize){
+		//Generates a hand based on handSize
+			
+	}
 	
 
 
