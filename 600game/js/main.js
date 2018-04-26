378b97302b3329bebdb609a3584daba2d67d19c8
FanciesFancyRats <magriffi1986@gmail.com>
2018-04-26 10:59:58 -0500

    working on the moving function

--- 600game/js/main.js
+++ 600game/js/main.js
@@ -37,13 +37,21 @@ var GameState = {
 },
 
 	create:function() {
-		this.displayString = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 200, '',{font: "144px Arial", fill:"#ffffff"});
+		this.gameScore = 0;
+		this.displayString = this.game.add.text(this.game.world.centerX, 10, '',{font: "144px Arial", fill:"#ffffff"});
 		this.displayString.inputEnabled = true;
+
+		this.scoreDisplay = this.game.add.text(1200 , 10, this.gameScore,{font:'40px Arial', fill:'#ffffff'});
+
+	
+		
+		this.displayScore();
 		this.displayString.events.onInputDown.add(this.submit, this);
 
 		this.letters = game.add.group();
 		this.hand = [];
 		this.displayArray = [];
+		
 		this.cons = "BCDFGHJKLMNPQRSTVWXYZ";
 		this.cons = this.cons.split("");
 		this.vowel = "AEIOU";
@@ -71,7 +79,7 @@ var GameState = {
 		return(this.dictionary.includes(guess));
 },
 	dealHand:function(handSize){
-		//Generates a hand based on handSize
+		//Generates a hand(array of letters) based on handSize
 		vowelNum = Math.ceil(handSize/3);
 		constNum = handSize - vowelNum;
 		for (i = 0; i < vowelNum; i++){
@@ -82,7 +90,7 @@ var GameState = {
 		}	
 },
 	createHand:function(){
-		//Takes the contents of this.hand and makes game objects letters
+		//Takes the contents of this.hand and makes game object letters
 		var letter;
 		for( i = 0; i < this.hand.length; i++){
 			letter = this.letters.create(0, 0, this.hand[i]);
@@ -98,8 +106,10 @@ var GameState = {
 
 },
 	addLetter:function(letter){
-		//Adds clicked letter to the displayArray
-		//console.log(letter.name)		
+		//Kills the block the user clicked on and adds it's value to an array to be displayed and an array of the letters
+		//currently being guessed
+		//Then calls the function to display the currently guessed letters
+
 		letter.kill();
 		this.displayArray.push(letter.value);
 		this.guessArray.push(letter.name);
@@ -107,9 +117,7 @@ var GameState = {
 
 },
 	displayGuess:function(){
-		//Clears the current displayed letters and then prints out the current displayArray
-		//look at showWord() in hangman
-		//TODO
+		//Displays the current letters from displayArray
 		this.displayString.kill();
 		this.s = this.displayArray.join('');
 		this.displayString = this.game.add.text(this.game.world.centerX, this.game.world.centerY - 200, this.s,{font: "144px Arial", fill:"#ffffff"});
@@ -118,21 +126,31 @@ var GameState = {
 		this.displayString.events.onInputDown.add(this.submit, this);
 		
 	},
+	displayScore:function(){
+		//TODO Make the final arangements to the layout
+		this.scoreDisplay.kill();
+		this.scoreDisplay = this.game.add.text(1200 , 20, this.gameScore,{font: '40px Arial', fill:'#ffffff'});
+		this.scoreDisplay.anchor.setTo(0.5);
+
+		this.scoreText = this.game.add.text(1100 , 20, 'Score:',{font: '40px Arial', fill:'#ffffff'});
+		this.scoreText.anchor.setTo(0.5);
+},
 	submit:function(){
 		//Called when displayString is clicked, checks this.string is a word and then kills the word
-		//if true gets score
+		//if true gets score and begin moving the letters
 		//if false revives letters
 
 		var target;
-		//console.log(this.s);
-		//console.log(this.checkWord(this.s));
 		if(this.checkWord(this.s)){
 			this.displayString.kill();
-			//console.log('you get points');
 			this.getScore();
+			this.displayScore();
 			this.s = '';
 			this.displayArray = [];
+			this.moveLetters();
 			this.guessArray = [];
+			
+		
 		}	
 		else{
 			this.displayString.kill();	
@@ -142,62 +160,85 @@ var GameState = {
 			}
 			this.s = '';
 			this.displayArray = [];
-			//console.log('Should be back');
 
 		}
 	
 	},
-	// 'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 	getScore:function(){
-		//TODO:
-		//Add a this.gameScore variable
-		//add other conditionals into the scoring, (hand size and possibly time)
-		//
-		//
-		//If the word is valid find the score of the word
+		//Takes a word and calculates the score and stores it in this.gameScore
 		wordScore = 0;
-		//console.log('yay points');
-		console.log(['A'].indexOf(this.displayArray[1]));
 		for(i = 0; i < this.displayArray.length; i++){
 			if(['A' , 'E' , 'I' , 'L' , 'N' , 'O' , 'R' , 'S' , 'T' , 'U'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 1;
-				console.log(this.displayArray[i], ['A' , 'E' , 'I' , 'L' , 'N' , 'O' , 'R' , 'S' , 'T' , 'U'].indexOf(this.displayArray[i]));
 			}
 
 			else if(['D' , 'G'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 2;	
-				console.log(this.displayArray[i], 'for 2');
 			}
 			else if(['B' , 'C' , 'M' , 'P'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 3;	
-				console.log(this.displayArray[i], 'for 3');
 			}
-			else if(['F' , 'H' , 'U' , 'W' , 'Y'].indexOf(this.displayArray[i]) >= 0){
+			else if(['F' , 'H' , 'U' ,'V', 'W' , 'Y'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 4;
-				console.log(this.displayArray[i], 'for 4');
 			}
 			else if(['K'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 5;	
-				console.log(this.displayArray[i], 'for 5');
 
 			}
 			else if(['J' , 'X'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 8;	
-				console.log(this.displayArray[i], 'for 8');
 
 			}
 			else if(['Q' , 'Z'].indexOf(this.displayArray[i]) >= 0){
 				wordScore+= 10;	
-				console.log(this.displayArray[i], 'for 10');
-
 			}
 
 
 		}
+		if((7*this.displayArray.length+1) - 3*((32/2)-this.displayArray.length+1) > 1){
+			wordScore+=((7*this.displayArray.length+1) - 3*((32/2)-this.displayArray.length+1))
+			this.moveLetters();
+
+		}
+		else{
+			wordScore++;		
+		}
 		console.log('for ',wordScore, 'points');
+		this.gameScore+=wordScore;
+		console.log('Total: ', this.gameScore);
+	},
+	moveLetters:function(){
+		//
+		for(i = 0; i < this.guessArray.length; i++){
+			swapTarget = 0;
+			swapOrign = 0
+			console.log(this.guessArray[i]);	
+			if(this.guessArray[i] + 8 < 32){
+				console.log(this.letters.getChildAt(this.guessArray[i]), 'needs moved');
+				//TODO
+				//while guessArray.length > 0
+				//	while target(guessArray(i + 8)) 
+				//This we can modify the .length of an array
+				//so something like i = array.length; i > 0; i--j
+				while(this.guessArray.length > 0){
+					swapOrgin = (this.guessArray[this.guessArray.length - 1]);
+					swapTarget = (this.guessArray[this.guessArray.length - 1] + 8);
 
 
+					while(swapTarget < 32) {
+						console.log(this.letters.getChildAt(swapTarget));
+						swapTarget += 8;
+						
+					}
+					this.guessArray.length--;	
+				}
+			}
+			else{
+				console.log('nothing to move');	
+			}
+		}			
 	}
+
 
 	
 
